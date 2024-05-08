fb4ee412bd6d8aef132c260c863d2c872f71ab36
nyakako <nyakako13@gmail.com>
2024-05-08 15:55:44 +0900

    section12-79まで

---
+++ src/components/organisms/user/UserDetailModal.tsx
@@ -0,0 +1,56 @@
+﻿import {
+	FormControl,
+	FormLabel,
+	Input,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalHeader,
+	ModalOverlay,
+	Stack,
+} from "@chakra-ui/react";
+import { FC, memo } from "react";
+
+type Props = {
+	isOpen: boolean;
+	onClose: () => void;
+};
+
+export const UserDetailModal: FC<Props> = memo((props) => {
+	const { isOpen, onClose } = props;
+	return (
+		<Modal
+			isOpen={isOpen}
+			onClose={onClose}
+			autoFocus={false}
+			motionPreset="slideInBottom"
+		>
+			<ModalOverlay />
+			<ModalContent pb={6}>
+				<ModalHeader>ユーザー詳細</ModalHeader>
+				<ModalBody mx={4}>
+					<Stack spacing={4}>
+						<FormControl>
+							<FormLabel>名前</FormLabel>
+							<Input value="じゃけぇ" isReadOnly />
+						</FormControl>
+						<FormControl>
+							<FormLabel>フルネーム</FormLabel>
+							<Input value="Takumi Okada" isReadOnly />
+						</FormControl>
+						<FormControl>
+							<FormLabel>MAIL</FormLabel>
+							<Input value="12345@example.com" isReadOnly />
+						</FormControl>
+						<FormControl>
+							<FormLabel>TEL</FormLabel>
+							<Input value="000-0000-0000" isReadOnly />
+						</FormControl>
+					</Stack>
+				</ModalBody>
+				<ModalCloseButton />
+			</ModalContent>
+		</Modal>
+	);
+});
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,44 @@
-﻿import { FC, memo } from "react";
+﻿/* eslint-disable react-hooks/exhaustive-deps */
+import {
+	Center,
+	Spinner,
+	Wrap,
+	WrapItem,
+	useDisclosure,
+} from "@chakra-ui/react";
+import { FC, memo, useCallback, useEffect } from "react";
+
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { UserCard } from "../organisms/user/UserCard";
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
 
 export const UserManagement: FC = memo(() => {
-	return <p>ユーザー管理ページです</p>;
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const { getUsers, users, loading } = useAllUsers();
+	useEffect(() => getUsers(), []);
+
+	const onClickUser = useCallback(() => onOpen(), []);
+	return (
+		<>
+			{loading ? (
+				<Center h="100vh">
+					<Spinner />
+				</Center>
+			) : (
+				<Wrap p={{ base: 4, md: 10 }}>
+					{users.map((user) => (
+						<WrapItem key={user.id} mx="auto">
+							<UserCard
+								imageUrl="https://source.unsplash.com/random"
+								userName={user.username}
+								fullName={user.name}
+								onClick={onClickUser}
+							/>
+						</WrapItem>
+					))}
+				</Wrap>
+			)}
+			<UserDetailModal isOpen={isOpen} onClose={onClose} />
+		</>
+	);
 });
